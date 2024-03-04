8a2dcebc83cd0c8272ea9112cef4eeb5a97ad4e2
StanMey <stan_1998@hotmail.com>
2024-03-05 00:34:23 +0100

    build the first training script

--- DVlog/train_dvlog_base.py
+++ DVlog/train_dvlog_base.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 # PyTorch TensorBoard support
-from torch.utils.tensorboard import SummaryWriter
+# from torch.utils.tensorboard import SummaryWriter
 from torch.utils.data import DataLoader
 from pathlib import Path
 
@@ -17,11 +17,12 @@ from models.unimodel import UnimodalDVlogModel
 # epochs: 50
 # learning rate: 0.0002
 # sequence length (t): 596
-EPOCHS = 10
+EPOCHS = 50
 BATCH_SIZE = 32
 LEARNING_RATE = 0.0002
 SEQUENCE_LENGTH = 596
 USE_GPU = True
+MODEL_NAME = "unimodal_acoustic_v1"
 
 
 # setup the paths
@@ -49,11 +50,15 @@ model = UnimodalDVlogModel(25, 5)
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
-
 # run the training
-for epoch in range(EPOCHS):  # loop over the dataset multiple times
-    print('EPOCH {}:'.format(epoch + 1))
+epoch_number = 0
+best_vloss = 1_000_000
 
+for epoch in range(EPOCHS):  # loop over the dataset multiple times
+    print('EPOCH {}:'.format(epoch_number + 1))
+
+    # make sure the gradient is on and do a pass over the data
+    model.train(True)
     running_loss = 0.0
     for i, data in enumerate(train_dataloader):
 
@@ -69,11 +74,35 @@ for epoch in range(EPOCHS):  # loop over the dataset multiple times
         loss.backward()
         optimizer.step()
 
-        # print statistics
+        # save statistics
         running_loss += loss.item()
-        if i % 10 == 9:    # print every 10 mini-batches
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 10))
-            running_loss = 0.0
+    avg_loss = running_loss / (i + 1)
+    
+    # Per-Epoch Activity
+    # Set the model to evaluation mode, disabling dropout and using population
+    # statistics for batch normalization.
+    running_vloss = 0.0
+    model.eval()
+
+    # Disable gradient computation and reduce memory consumption.
+    with torch.no_grad():
+        for i, vdata in enumerate(val_dataloader):
+            _, v_acoustic_inputs, vlabels = vdata
+
+            voutputs = model(v_acoustic_inputs)
+            vloss = criterion(voutputs, vlabels)
+            running_vloss += vloss
+    
+    avg_vloss = running_vloss / (i + 1)
+    print('LOSS train {} valid {}'.format(avg_loss, avg_vloss))
+
+    # Track best performance, and save the model's state
+    if avg_vloss < best_vloss:
+        best_vloss = avg_vloss
+        model_path = 'model_{}_{}'.format(MODEL_NAME, epoch_number)
+        torch.save(model.state_dict(), model_path)
+    
+    epoch_number += 1
+
 
 print('Finished Training')
